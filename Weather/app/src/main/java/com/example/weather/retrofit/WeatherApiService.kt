b03225570572e82a0b3bb924e9a20922db528f6e
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-03-07 15:11:02 +0530

    query params added for api calls

--- Weather/app/src/main/java/com/example/weather/retrofit/WeatherApiService.kt
+++ Weather/app/src/main/java/com/example/weather/retrofit/WeatherApiService.kt
@@ -4,6 +4,7 @@ import com.example.weather.data.ForeCaseWeather
 import com.example.weather.data.WeatherData
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WeatherApiService {
 
@@ -11,16 +12,22 @@ interface WeatherApiService {
      * This function gets the current location
      *
      */
-    @GET("data/2.5/weather?q=Bengaluru&APPID=9b8cb8c7f11c077f8c4e217974d9ee40")
-    suspend fun getSpecificWeather(): Response<WeatherData>
+    @GET("data/2.5/weather")
+    suspend fun getSpecificWeather(
+        @Query("q") cityName: String = "Bengaluru",
+        @Query("APPID") apiKey: String = "9b8cb8c7f11c077f8c4e217974d9ee40"
+    ): Response<WeatherData>
 
 
     /**
      * This function gets the forecast weather
      *
      */
-    @GET("data/2.5/forecast?q=Bengaluru&APPID=9b8cb8c7f11c077f8c4e217974d9ee40")
-    suspend fun getForeCastWeather(): Response<ForeCaseWeather>
+    @GET("data/2.5/forecast")
+    suspend fun getForeCastWeather(
+        @Query("q") cityName: String = "Bengaluru",
+        @Query("APPID") apiKey: String = "9b8cb8c7f11c077f8c4e217974d9ee40"
+    ): Response<ForeCaseWeather>
 
 }
 
